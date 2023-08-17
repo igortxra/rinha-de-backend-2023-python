cfea19d13eb6b9f944fad91abc327ceca1613d70
igortxra <igor.teixeira@cromai.com>
2023-08-17 20:15:25 -0300

    feat: add create person validations

--- app/main.py
+++ app/main.py
@@ -1,22 +1,36 @@
+import uuid
+from datetime import datetime
 from typing import List
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, constr, validator
 
 # Modelos
-class Pessoa(BaseModel):
-    id: str
-    apelido: str
-    nome: str
+class PessoaBase(BaseModel):
+    apelido: constr(max_length=32)
+    nome: constr(max_length=100)
     nascimento: str
-    stack: List[str]
+    stack: List[constr(max_length=32)] | None
+
+    @validator("nascimento")
+    def valida_formato_data(cls, valor):
+        try:
+            datetime.strptime(valor, '%Y-%m-%d')
+            return valor
+        except ValueError:
+            raise ValueError("Formato de data inválido")
+
+class PessoaRead(PessoaBase):
+    id: str | None
 
 # API e Rotas
 app = FastAPI()
 
 @app.post("/pessoas")
-async def create_person():
-    return {"message": "This action create a person"}
-
+async def create_person(person: PessoaBase):
+    person = PessoaRead(id=str(uuid.uuid4()), **dict(person))
+    # ok = insere_pessoa
+    # if ok:
+    return person
 
 @app.get("/pessoas")
 async def get_persons(t: str = None):
