a748e3e077b4dc795a857490ff332de7f057f148
igortxra <igor.teixeira@cromai.com>
2023-08-17 18:58:51 -0300

    chore: add basic structure and requirements

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.post("/pessoas")
+async def create_person():
+    return {"message": "This action create a person"}
+
+
+@app.get("/pessoas")
+async def get_persons(t: str):
+    return {"message": f"This action return all peoples"}
+
+
+@app.get("/contagem-pessoas")
+async def get_count():
+    return {"message": "This action return count of peoples"}
+
